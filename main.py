27a6734f5dde6e570b66cbe33db2f981279e05b5
Aidan Clyens <aidanc@rogers.com>
2019-02-07 12:56:38 -0500

    Added UW Flow URLs and added cls, tut, and lab credits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+uwflow_url = "https://uwflow.com/"
 calendar_url = "https://ugradcalendar.uwaterloo.ca/"
 ece_page = "page/ENG-Computer-Engineering-Electrical-Engineering"
 
@@ -19,11 +20,14 @@ class Course:
     name = ''
     code = ''
     program = ''
+    cls = 0
+    tut = 0
+    lab = 0
     url = ''
+    uwflow_url = ''
 
-    def __init__(self, name, code,  program):
+    def __init__(self, name, code):
         self.name = name
-        self.program = program
         self.code = code
 
 def get_site_content(url):
@@ -32,11 +36,10 @@ def get_site_content(url):
 
     soup = BeautifulSoup(text, 'html.parser')
 
+    return soup
+
+def get_course_list(soup):
     main_content = soup.find('span', {'class':'MainContent'})
-
-    return main_content
-
-def get_course_list(main_content):
     table = main_content.find('table')
 
     rows = table.find_all('tr')
@@ -53,14 +56,27 @@ def get_courses_from_list(rows):
                 code = cols[2].text.strip()
                 program = cols[1].text.strip()
                 academic_term = cols[0].text.strip()[14:16]
+                
+                if len(cols) > 6:
+                    cls = float(cols[4].text.strip())
+                    tut = float(cols[5].text.strip())
+                    lab = float(cols[6].text.strip())
             else:
                 course_name = cols[2].text.strip()
                 code = cols[1].text.strip()
                 program = cols[0].text.strip()
-            
-            if not course_name == 'n/a':
-                course = Course(course_name, code, program)
-                terms[academic_term].append(course)
+
+                if len(cols) > 5:
+                    cls = float(cols[3].text.strip())
+                    tut = float(cols[4].text.strip())
+                    lab = float(cols[5].text.strip())
+    
+            course = Course(course_name, code)
+            course.program = program
+            course.cls = cls
+            course.tut = tut
+            course.lab = lab
+            terms[academic_term].append(course)
 
 def add_course_urls(year):
     course_list_url = "http://www.ucalendar.uwaterloo.ca/" + (year[2:5] + str((int(year[2:5]) + 1))) + "/COURSE/course-ECE.html"
@@ -68,6 +84,7 @@ def add_course_urls(year):
     for term in terms.values():
         for course in term:
             course.url = course_list_url + "#" + course.code.replace(' ', '')
+            course.uwflow_url = uwflow_url + "course/" + course.code.replace(' ', '').lower()
 
 def get_courses(year):
     url = calendar_url + ece_page + "/?ActiveDate=9/1/" + year
@@ -89,9 +106,12 @@ def main():
     print url, "\n"
 
     term = raw_input("Please select a term: ")
-
-    for course in terms[term]:
-        print course.code, course.url
+    
+    if term in terms.keys():
+        for course in terms[term]:
+            print course.code, "  ", course.url, "  ",  course.uwflow_url, "  ", course.cls, "  ", course.tut, "  ", course.lab
+    else:
+        print "Invalid key:", term
 
 if __name__ == '__main__':
     main()

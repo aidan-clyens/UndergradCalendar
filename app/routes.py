1a868a99a6696de09e4348c54ac3cd7f22cd48bd
Aidan Clyens <aidanc@rogers.com>
2019-02-19 22:19:44 -0500

    Only support 2016 starting year for production

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,11 @@ from . import webscraper
 from datetime import datetime
 import os
 
-min_year = 2012
-max_year = datetime.now().year
+year = "2016"
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        year = request.form['year']
+    ws = webscraper.WebScraper()
+    terms = ws.get_courses(year)
 
-        ws = webscraper.WebScraper()
-        terms = ws.get_courses(year)
-
-        if int(year) >= min_year and int(year) <= max_year:
-            return render_template('courses.html', terms=terms)
-
-    return render_template('index.html')
+    return render_template('courses.html', terms=terms)

22cbe79e8f7540436889b0613fe563e0f82b69b4
Aidan Clyens <aidanc@rogers.com>
2019-02-25 12:51:25 -0500

    Remove db from dark-theme branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
-app.config.from_mapping(
-    DATABASE='app/courses.db'
-)
 
 from app import routes
-from app import db
-
-db.init_app(app)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from app import app
 from flask import request, render_template
 from . import webscraper
-from . import db
 from datetime import datetime
 import os
 
@@ -10,21 +9,13 @@ max_year = datetime.now().year
 
 @app.route('/', methods=['GET','POST'])
 def index():
-
-    res = db.init_db()
-
     if request.method == 'POST':
         year = request.form['year']
 
-        select_sql = """SELECT * FROM courses WHERE start_year=%s""" % year
-        res = db.query_db(select_sql)
-        if len(res) > 0:
-            terms = db.get_courses_from_db(year)
-        else:
+        if int(year) >= min_year and int(year) <= max_year:
             ws = webscraper.WebScraper()
             terms = ws.get_courses(year)
 
-        if int(year) >= min_year and int(year) <= max_year:
             return render_template('courses.html', terms=terms)
 
     return render_template('index.html')

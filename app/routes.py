ce03f32a47c1b7f1eeb24289adee947e6bb0600f
Aidan Clyens <aidanc@rogers.com>
2019-03-13 14:11:35 -0400

    Use cookies to set theme

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import request, render_template
+from flask import request, render_template, make_response
 from . import webscraper
 from datetime import datetime
 import os
@@ -8,6 +8,15 @@ min_year = 2012
 max_year = datetime.now().year
 
 @app.route('/', methods=['GET','POST'])
+def reload():
+    dark_theme = request.cookies.get('dark_theme')
+    if dark_theme == 'dark': dark_theme = True
+    elif dark_theme == 'light': dark_theme = False
+    else: dark_theme = False
+
+    return index(dark_theme)
+
+@app.route('/light', methods=['GET','POST'])
 def light():
     return index(False)
 
@@ -25,4 +34,10 @@ def index(dark_theme=False):
 
             return render_template('courses.html', terms=terms, url=ws.url, dark_theme=dark_theme)
 
-    return render_template('index.html', dark_theme=dark_theme)
+    resp = make_response(render_template('index.html', dark_theme=dark_theme))
+    if dark_theme: dark_theme = 'dark'
+    else: dark_theme = 'light'
+    
+    resp.set_cookie('dark_theme', dark_theme)
+
+    return resp
